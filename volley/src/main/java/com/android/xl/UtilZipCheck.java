04263cefda56806084beddec6e21a6f098d8368f
xilinch <308825747@qq.com>
2017-09-20 10:49:50 +0800

    1.修改 间距 2.优化文件释放

--- volley/src/main/java/com/android/xl/UtilZipCheck.java
+++ volley/src/main/java/com/android/xl/UtilZipCheck.java
@@ -14,14 +14,27 @@ public class UtilZipCheck {
     public static boolean isErrorZip(String filePath){
         File file = new File(filePath);
         boolean isRight = true;
+        ZipFile zipFile = null;
         try{
-            ZipFile zipFile = new ZipFile(file);
+            zipFile = new ZipFile(file);
         } catch (Exception exception) {
             exception.printStackTrace();
             Log.e("my","installAPK zipFile error");
             isRight = false;
             if(file != null && file.exists()){
                 file.delete();
+            }
+        } finally {
+            if(zipFile != null){
+                try {
+                    zipFile.close();
+                } catch (Exception exception) {
+                    exception.printStackTrace();
+
+                } finally {
+                    zipFile = null;
+                }
+
             }
         }
         return isRight;
